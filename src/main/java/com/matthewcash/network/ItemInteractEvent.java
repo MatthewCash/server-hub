279db49acc266262e87bfcc78f5a631898b6d757
Matthew_Cash <matthew@matthew-cash.com>
2020-09-30 17:57:45 -0700

    Prevent interact event running on physical events

--- src/main/java/com/matthewcash/network/ItemInteractEvent.java
+++ src/main/java/com/matthewcash/network/ItemInteractEvent.java
@@ -4,12 +4,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ItemInteractEvent implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onItemClick(PlayerInteractEvent event) {
+        // Cancel if event is not a click
+        if (event.getAction() == Action.PHYSICAL) {
+            return;
+        }
+
         Player player = event.getPlayer();
         Integer currentSlot = player.getInventory().getHeldItemSlot();
 
