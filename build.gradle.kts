5a405de6d42af6d21cfbc331a94c2afade6785c3
Matthew_Cash <matthew@matthew-cash.com>
2023-01-22 13:03:51 -0800

    Fix build dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,16 @@ plugins {
 }
 
 repositories {
-    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
-        name = "spigot-repo"
-    }
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots") {
-        name = "sonatype"
-    }
-    maven(url = "https://repo.citizensnpcs.co/") {
-        name = "everything"
-    }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://maven.citizensnpcs.co/repo") }
+    maven { url = uri("https://repo.alessiodp.com/releases/") }
     mavenCentral()
 }
 
 dependencies {
     implementation("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
-    implementation("net.citizensnpcs:citizens:2.0.30-SNAPSHOT")
+    implementation("net.citizensnpcs:citizens-main:2.0.30-SNAPSHOT")
 }
 
 group = "com.matthewcash.network"
