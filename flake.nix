a8bc037c83543bf252c4a2b36edfb0aa126814ef
Matthew_Cash <matthew@matthew-cash.com>
2023-06-12 21:22:13 -0700

    Cleanup flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,13 @@
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-unstable";
-
-        flake-utils = {
-            url = "github:numtide/flake-utils";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = inputs @ { self, nixpkgs, flake-utils, ... }:
+    outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in rec {
-            devShell = pkgs.mkShell rec {
+        let pkgs = nixpkgs.legacyPackages.${system}; in {
+            devShell = pkgs.mkShell {
                 name = "server-hub";
                 packages = with pkgs; [
                     gradle
